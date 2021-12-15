5f368bbde661736699c2eadf1164fc33042ab2ea
wilker123 <josewilkerlucena123@gmail.com>
2021-12-15 14:13:12 -0300

    Modify image card course

--- project_responsive/lib/Pages/home/widgets/course_item.dart
+++ project_responsive/lib/Pages/home/widgets/course_item.dart
@@ -6,8 +6,9 @@ class CourseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.network("https://miro.medium.com/max/700/1*73IgUxPfyXUKZAaIXgutrw.png"),
+        Image.network("https://miro.medium.com/max/700/1*73IgUxPfyXUKZAaIXgutrw.png", fit: BoxFit.fitWidth,),
         const SizedBox(height: 4,),
         const Text(
           "Criação de apps android e ios com flutter - Crie 16 apps",
