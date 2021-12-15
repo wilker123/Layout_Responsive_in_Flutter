0571d3e47611890645c7d936d0ab712940184949
wilker123 <josewilkerlucena123@gmail.com>
2021-12-15 14:55:43 -0300

    Adding and configuring package device_preview. Test responsive

--- project_responsive/lib/main.dart
+++ project_responsive/lib/main.dart
@@ -1,11 +1,25 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 import 'Pages/home/home_page.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Projeto Layout Responsivo',
-    home: HomePage(),
-  ));
+  runApp(DevicePreview(builder: (_) => MyApp()));
 }
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Projeto Layout Responsivo',
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
+      home: HomePage(),
+    );
+  }
+}
+
