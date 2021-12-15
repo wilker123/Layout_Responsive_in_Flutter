f75353c1a52cc572aea75c5e7054f943dc18b4ae
wilker123 <josewilkerlucena123@gmail.com>
2021-12-15 14:04:37 -0300

    Created title card course

--- project_responsive/lib/Pages/home/widgets/course_item.dart
+++ project_responsive/lib/Pages/home/widgets/course_item.dart
@@ -7,7 +7,12 @@ class CourseItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Image.network("https://miro.medium.com/max/700/1*73IgUxPfyXUKZAaIXgutrw.png")
+        Image.network("https://miro.medium.com/max/700/1*73IgUxPfyXUKZAaIXgutrw.png"),
+        const SizedBox(height: 4,),
+        const Text(
+          "Criação de apps android e ios com flutter - Crie 16 apps",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.white),
+        ),
       ],
     );
   }
